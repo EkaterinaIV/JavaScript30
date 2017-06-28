d56aaca2645edc0406803334ab88d468c9c8a4bf
Katie Yves <EkaterinaIV@users.noreply.github.com>
2017-06-28 19:30:28 +0300

    task 3

--- 02 - JS and CSS Clock/main.js
+++ 02 - JS and CSS Clock/main.js
@@ -8,14 +8,23 @@ function setDate() {
     const seconds = now.getSeconds();
     const secondsDegree = (seconds/60) * 360 + 90;
     secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+    if (seconds === 0) {
+        secondHand.style.transition = "none";
+    };
 
     const minutes = now.getMinutes();
     const minutesDegree = (minutes/60) * 360 + 90;
     minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
+    if (minutes === 0) {
+        minuteHand.style.transition = "none";
+    };
 
     const hours = now.getHours();
     const hoursDegree = (hours/12) * 360 + 90;
     hourHand.style.transform = `rotate(${hoursDegree})`;
+    if (hours === 0) {
+        hourHand.style.transition = "none";
+    };
 };
 
 setInterval(setDate, 1000);
